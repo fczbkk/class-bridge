d3f600a0e65f5047e23258c05a83ed3b61653c09
Riki Fridrich <riki@fczbkk.com>
2015-12-21 19:57:44 +0100

    Release v2.1.0

--- build/class-bridge.js
+++ build/class-bridge.js
@@ -6,25 +6,56 @@
   };
 
   ClassBridge = {
+    _addNamespace: function(classname) {
+      return classname;
+    },
     has: function(element, classname) {
+      classname = this._addNamespace(classname);
       return constructRe(classname).test(element.className);
     },
     add: function(element, classname) {
+      classname = this._addNamespace(classname);
       if (!this.has(element, classname)) {
         return element.className += " " + classname;
       }
     },
     remove: function(element, classname) {
       var re;
+      classname = this._addNamespace(classname);
       re = constructRe(classname);
       return element.className = element.className.replace(re, ' ');
     },
-    toggle: function(element, classname) {
-      if (this.has(element, classname)) {
-        return this.remove(element, classname);
+    toggle: function(element, classname, condition) {
+      classname = this._addNamespace(classname);
+      if (condition != null) {
+        if (condition) {
+          return this.add(element, classname);
+        } else {
+          return this.remove(element, classname);
+        }
       } else {
-        return this.add(element, classname);
+        if (this.has(element, classname)) {
+          return this.remove(element, classname);
+        } else {
+          return this.add(element, classname);
+        }
       }
+    },
+    withNamespace: function(namespace) {
+      var clone, key, val;
+      if (namespace == null) {
+        namespace = '';
+      }
+      clone = {};
+      for (key in ClassBridge) {
+        val = ClassBridge[key];
+        clone[key] = val;
+      }
+      delete clone.withNamespace;
+      clone._addNamespace = function(classname) {
+        return namespace + classname;
+      };
+      return clone;
     }
   };
 
